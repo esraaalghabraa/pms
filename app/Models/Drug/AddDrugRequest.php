da128203db06b4734a4087b8061c175fa111a31a
Esraa Alghabra <102614216+esraalghabra@users.noreply.github.com>
2023-07-15 11:41:33 +0200

    AddDrugRequest from repository to admin

---
+++ app/Models/Drug/AddDrugRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Drug;
+
+use App\Models\Registration\Repository;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AddDrugRequest extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded=[];
+
+    function repository() : BelongsTo{
+        return $this->belongsTo(Repository::class,'repository_id');
+    }
+}
