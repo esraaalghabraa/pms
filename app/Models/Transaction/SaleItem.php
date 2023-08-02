84d6679964cdb8d99185f4a7a0cc2f55729a7859
Esraa Alghabra <102614216+esraalghabra@users.noreply.github.com>
2023-08-02 17:03:29 +0200

    add relation between pharmacyBatch and SaleItem

--- app/Models/Transaction/SaleItem.php
+++ app/Models/Transaction/SaleItem.php
@@ -4,17 +4,19 @@ namespace App\Models\Transaction;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SaleItem extends Model
 {
     use HasFactory;
+    protected $guarded=[];
 
-    public function saleBill()
+    public function saleBill():BelongsTo
     {
         return $this->belongsTo(SaleBill::class);
     }
-    public function pharmacyStorage()
+    public function pharmacyBatch():BelongsTo
     {
-        return $this->belongsTo(PharmacyStorage::class);
+        return $this->belongsTo(PharmacyBatch::class);
     }
 }
