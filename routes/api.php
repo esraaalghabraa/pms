e457d5ebcc78cdd9c3ef0c359b2cd8d76894d323
Esraa Alghabra <102614216+esraalghabra@users.noreply.github.com>
2023-08-02 17:11:42 +0200

    add routes for customers and employees and roles and sale-bills

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 
-use App\Http\Controllers\Repository\RepositoryController;
 use App\Http\Controllers\User\Auth\AuthUserController;
+use App\Http\Controllers\User\Pharmacy\CustomerController;
+use App\Http\Controllers\User\Pharmacy\EmployeeController;
 use App\Http\Controllers\User\Pharmacy\MedicinesBuyOrderController;
 use App\Http\Controllers\User\Pharmacy\PharmacyMedicinesController;
 use App\Http\Controllers\User\Pharmacy\RepositoriesController;
+use App\Http\Controllers\User\Pharmacy\RoleController;
+use App\Http\Controllers\User\Pharmacy\SaleBillsController;
 use App\Http\Controllers\User\Repository\PharmaciesController;
 use App\Http\Controllers\User\Repository\RepositoryMedicinesController;
 use App\Http\Controllers\User\Requests\MedicinesController;
@@ -33,14 +36,14 @@ Route::controller(AuthUserController::class)
         Route::get('login_with_token', 'loginWithToken');
         Route::post('send-verify-code', 'sendVerifyCode');
         Route::post('verify-code', 'verifyCode');
-        Route::middleware(['auth:sanctum', 'abilities:frontuser'])
+        Route::middleware(['auth:sanctum', 'abilities:user'])
             ->group(function () {
                 Route::get('logout', 'logout');
                 Route::post('add_info', 'addInfo');
             });
     });
 
-Route::middleware(['auth:sanctum', 'abilities:frontuser'])->group(function () {
+Route::middleware(['auth:sanctum', 'abilities:user'])->group(function () {
 
     Route::prefix('pharmacy')->group(function () {
         Route::prefix('stored-medicines')->controller(PharmacyMedicinesController::class)
@@ -65,6 +68,39 @@ Route::middleware(['auth:sanctum', 'abilities:frontuser'])->group(function () {
                 Route::post('send', 'sendOrder');
                 Route::post('get-repository', 'getRepository');
             });
+        Route::prefix('customers')->controller(CustomerController::class)
+            ->group(function (){
+                Route::post('get-all','getAll');
+                Route::post('create','create');
+                Route::post('saleBile','saleBile');
+                Route::post('update','update');
+                Route::post('delete','delete');
+                Route::post('get','getCustomer');
+            });
+        Route::prefix('employees')->controller(EmployeeController::class)
+            ->group(function (){
+                Route::post('get-all','getAll');
+                Route::post('delete','delete');
+                Route::post('get','get');
+                Route::post('create','create');
+            });
+        Route::prefix('roles')->controller(RoleController::class)
+            ->group(function (){
+                Route::post('get-all','getAll');
+                Route::post('create','createRole');
+                Route::post('update','update');
+                Route::post('createe','createPermission');
+                Route::post('destroy','destroy');
+            });
+        Route::prefix('sale-bills')->controller(SaleBillsController::class)
+            ->group(function (){
+                Route::post('get-all-daily','getDailyBills');
+                Route::post('get-all-customers','getCustomerBills');
+                Route::post('create','create');
+                Route::post('update','update');
+                Route::post('delete','delete');
+                Route::post('get','getBill');
+            });
     });
 
     Route::prefix('repository')->group(function () {
@@ -83,10 +119,7 @@ Route::middleware(['auth:sanctum', 'abilities:frontuser'])->group(function () {
             Route::post('get-pharmacy', 'getPharmacy');
         });
 
-        Route::controller(RepositoryController::class)->group(function () {
-            Route::post('create-drug-storage', 'createDrugStorage');
-            Route::post('create-batch', 'createBatchDrug');
-        });
+
     });
 
     Route::prefix('medicines')->controller(MedicinesController::class)
@@ -95,18 +128,7 @@ Route::middleware(['auth:sanctum', 'abilities:frontuser'])->group(function () {
             Route::post('search', 'searchMedicines');
         });
 
-    Route::controller(RequestsUserController::class)->prefix('requests')
-        ->middleware(['auth:sanctum', 'abilities:frontuser'])->group(function () {
-            Route::get('get-requests', 'getRequests');
-            Route::get('get-archived-requests', 'getArchivedRequests');
-            Route::post('create-request', 'createRequestRegistration');
-            Route::post('reject-request', 'rejectRequest');
-            Route::post('accept-request', 'acceptRequest');
-            Route::post('delete-request', 'deleteRequest');
-        });
-
-    Route::controller(RequestsUserController::class)
-        ->prefix('requests')->group(function () {
+    Route::controller(RequestsUserController::class)->prefix('requests')->group(function () {
             Route::get('get-requests', 'getRequests');
             Route::get('get-archived-requests', 'getArchivedRequests');
             Route::post('create-request', 'createRequestRegistration');
