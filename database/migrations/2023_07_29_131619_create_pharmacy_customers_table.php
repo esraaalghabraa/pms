670ab8001e34e6afff5f7bf93f8945060eeea42d
Esraa Alghabra <102614216+esraalghabra@users.noreply.github.com>
2023-08-02 17:10:46 +0200

    add pharmacy_customers table

--- database/migrations/2023_07_29_131619_create_pharmacy_customers_table.php
+++ database/migrations/2023_07_29_131619_create_pharmacy_customers_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('permissions', function (Blueprint $table) {
+        Schema::create('pharmacy_customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->foreignId('pharmacy_id')->constrained('pharmacies');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('pharmacy_customers');
     }
 };
